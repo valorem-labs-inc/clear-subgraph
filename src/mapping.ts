96f3885e707e37f935e240433594932982c42531
Alcibiades <89996683+0xAlcibiades@users.noreply.github.com>
2022-06-01 15:40:58 -0400

    Ability to query options ERC-1155 tokens
    * trim cruft
    * ability to options holdings

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,7 +1,6 @@
 import { BigInt, ethereum } from "@graphprotocol/graph-ts"
 import {
   OptionsSettlementEngine,
-  ApprovalForAll,
   ClaimRedeemed,
   ExerciseAssigned,
   FeeAccrued,
@@ -12,13 +11,11 @@ import {
   URI
 } from "../generated/OptionsSettlementEngine/OptionsSettlementEngine"
 
-import { Account, Balance, Claim, ExampleEntity, Option, Token, TokenRegistry, Transaction, Transfer } from "../generated/schema"
+import { Account, Balance, Claim, Option, Token, TokenRegistry, Transaction, Transfer } from "../generated/schema"
 
 import {
   TransferBatch as TransferBatchEvent,
   TransferSingle as TransferSingleEvent,
-  URI as URIEvent,
-  ApprovalForAll as ApprovalForAllEvent,
 } from '../generated/OptionsSettlementEngine/IERC1155';
 
 import { IERC1155MetadataURI } from '../generated/OptionsSettlementEngine/IERC1155MetadataURI';
@@ -104,63 +101,6 @@ function registerTransfer(
 }
 
 
-
-export function handleApprovalForAll(event: ApprovalForAll): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count.plus(BigInt.fromI32(1))
-
-  // Entity fields can be set based on event parameters
-  entity.owner = event.params.owner
-  entity.operator = event.params.operator
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.balanceOf(...)
-  // - contract.balanceOfBatch(...)
-  // - contract.claim(...)
-  // - contract.feeBalance(...)
-  // - contract.feeBps(...)
-  // - contract.feeTo(...)
-  // - contract.hashToOptionToken(...)
-  // - contract.isApprovedForAll(...)
-  // - contract.newChain(...)
-  // - contract.option(...)
-  // - contract.supportsInterface(...)
-  // - contract.tokenType(...)
-  // - contract.underlying(...)
-  // - contract.uri(...)
-  // - contract.write(...)
-}
-
 export function handleClaimRedeemed(event: ClaimRedeemed): void {
   let claim = Claim.load(event.params.claimId.toString());
 
@@ -203,19 +143,29 @@ export function handleFeeSwept(event: FeeSwept): void {
 
 export function handleNewChain(event: NewChain): void {
   let option = Option.load(event.params.optionId.toString());
+  let registry = new TokenRegistry(event.address.toHex());
 
     if (option == null) {
         option = new Option(event.params.optionId.toString());
     }
 
-    option.underlyingAsset = event.params.underlyingAsset;
-    option.exerciseTimestamp = event.params.exerciseTimestamp;
-    option.expiryTimestamp = event.params.expiryTimestamp;
-    option.exerciseAsset = event.params.exerciseAsset;
-    option.underlyingAmount = event.params.underlyingAmount;
-    option.exerciseAmount = event.params.exerciseAmount;
+  let token = fetchToken(registry, event.params.optionId);
+  let engine = OptionsSettlementEngine.bind(event.address);
+    // How to make a call here?
+  let callResult = engine.try_tokenType(event.params.optionId);
+  if (!callResult.reverted) {
+    token.type = callResult.value;
+    token.save();
+  }
 
-    option.save();
+  option.underlyingAsset = event.params.underlyingAsset;
+  option.exerciseTimestamp = event.params.exerciseTimestamp;
+  option.expiryTimestamp = event.params.expiryTimestamp;
+  option.exerciseAsset = event.params.exerciseAsset;
+  option.underlyingAmount = event.params.underlyingAmount;
+  option.exerciseAmount = event.params.exerciseAmount;
+
+  option.save();
 }
 
 export function handleOptionsExercised(event: OptionsExercised): void {
@@ -240,15 +190,11 @@ export function handleOptionsWritten(event: OptionsWritten): void {
       }
 
       // option written and now is able to have anyone use it
-      option.writer = event.params.writer
       option.claimId = event.params.claimId
       option.amount = event.params.amount
 
       option.save();
 }
-
-
-
 
 
 export function handleTransferBatch(event: TransferBatchEvent): void {
